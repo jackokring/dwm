69345bd9d3300801e323d5ba12df9354c282da4d
jackokring <jackokring@gmail.com>
2024-07-16 01:27:01 +0000

    ok, getting better

--- config.def.h
+++ config.def.h
@@ -5,6 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+// better, larger nerd font
 static const char *fonts[]          = { "JetBrainsMonoNL Nerd Font Mono:size=13" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -17,6 +18,7 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+// some pretty
 /* tagging */
 static const char *tags[] = { "1", "2󰗑", "3󰯙", "4󰄕", "5󱙧", "6󰾡", "7󰅣", "8", "9󱑓" };
 
@@ -25,10 +27,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Chrome",   NULL,       NULL,       1 << 8,       0,           -1 },
-	{ "Rofi",     NULL,       NULL,       0,            1,           -1 },
+	/* class      		instance    title       tags mask     isfloating   monitor */ //on tag <<
+	{ "Gimp",     		NULL,       NULL,       1 << 2,       0,           -1 },	//3
+	{ "Google-chrome",   	NULL,       NULL,       1 << 6,       0,           -1 },	//7
 };
 
 /* layout(s) */
@@ -60,12 +61,18 @@ static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() 
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "combi", NULL };
+static const char *googlecmd[]= { "google-chrome", NULL };
+static const char *gimpcmd[]  = { "gimp", NULL };
+static const char *emacscmd[] = { "emacs", "~", NULL };
 
 static const Key keys[] = {
-	// aegnosuvwxyz
+	// anosuvwxyz
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,	                XK_r,      spawn,          {.v = roficmd } },
+	{ MODKEY,	                XK_g,      spawn,          {.v = googlecmd } },
+	{ MODKEY|ShiftMask,	        XK_g,      spawn,          {.v = gimpcmd } },
+	{ MODKEY,                       XK_e,      spawn,          {.v = emacscmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
