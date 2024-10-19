bfb0f07010448db2e1a1a2e1ea5e20f2bb0f3558
jackokring <jackokring@gmail.com>
2024-10-19 12:35:11 +0000

    Saturday 2024-10-19 12:35:11

--- config.def.h
+++ config.def.h
@@ -83,7 +83,7 @@ static const char *termcmd[]  = { "st", NULL };
 static const char *roficmd[]  = { "rofi", "-show", "combi", NULL };
 static const char *googlecmd[]= { "google-chrome", NULL };//nmtui for net
 static const char *gimpcmd[]  = { "gimp", NULL };
-static const char *emacscmd[] = { "emacs", "~", NULL };
+static const char *nvimcmd[] = { "st", "nvim", NULL };
 
 #include "movestack.c"
 static const Key keys[] = {
@@ -94,7 +94,7 @@ static const Key keys[] = {
 	{ MODKEY,	                XK_r,      spawn,          {.v = roficmd } },
 	{ MODKEY,	                XK_c,      spawn,          {.v = googlecmd } },// no shift c
 	{ MODKEY,		        XK_g,      spawn,          {.v = gimpcmd } },// no shift g
-	{ MODKEY,                       XK_e,      spawn,          {.v = emacscmd } },
+	{ MODKEY,                       XK_v,      spawn,          {.v = nvimcmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -103,7 +103,7 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-       	{ MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
+    { MODKEY|ShiftMask,             XK_j,      movestack,      {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_k,      movestack,      {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_h,      setcfact,       {.f = +0.25} },
 	{ MODKEY|ShiftMask,             XK_l,      setcfact,       {.f = -0.25} },
